9e6a67d1035969ddd1809c00ad062a094612c1f5
BlueWhale <166388498+LewisLim-USA@users.noreply.github.com>
2024-12-06 13:20:43 +0800

    New controller and ui

---
+++ Teamwork_Lewis_CoreFunctions_FitnessGoal/src/main/java/com/example/week11_lab9_javafx/controller/form/FitnessGoalsController.java
@@ -0,0 +1,4 @@
+package com.example.week11_lab9_javafx.controller.form;
+
+public class FitnessGoalsController {
+}
---
+++ Teamwork_Lewis_CoreFunctions_FitnessGoal/src/main/java/com/example/week11_lab9_javafx/controller/form/ProgressVisualizationController.java
@@ -0,0 +1,6 @@
+package com.example.week11_lab9_javafx.controller.form;
+
+public class ProgressVisualizationController {
+
+
+}
