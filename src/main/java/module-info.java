f76d182c4a339acb6b8dcdaac9f15795a6fccc9f
zhenming22 <zmyong22@gmail.com>
2024-12-10 17:33:35 +0800

    My work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.teamworklewis {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.json;
 
 
     opens com.example.teamworklewis to javafx.fxml;
